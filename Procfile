526646de692e6528bcb4f8f6942b5ed52a8301b4
Scholigence-2026 <scholigence@gmail.com>
2026-04-13 07:07:29 +0530

    Fix Render gunicorn path for nested Django project

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn api_test_server.wsgi:application
+web: gunicorn api_test_server.api_test_server.wsgi:application
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 set -o errexit
 
 export PORT=${PORT:-10000}
-exec gunicorn api_test_server.wsgi:application --bind 0.0.0.0:$PORT
+exec gunicorn api_test_server.api_test_server.wsgi:application --bind 0.0.0.0:$PORT
