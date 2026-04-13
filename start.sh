fe0115ac98a1b5d53eeea7c453709f350ef0b288
Scholigence-2026 <scholigence@gmail.com>
2026-04-13 07:28:26 +0530

    Run gunicorn from Django project directory for correct WSGI path

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 set -o errexit
 
 export PORT=${PORT:-10000}
-exec gunicorn api_test_server.api_test_server.wsgi:application --bind 0.0.0.0:$PORT
+cd api_test_server
+exec gunicorn api_test_server.wsgi:application --bind 0.0.0.0:$PORT
